565764a3820b71c244e537c9ce83cef1dcf06bee
Brendan Long <blong145@gmail.com>
2017-02-11 10:03:22 -0600

    Added another intellisense example - misspelling properties

--- ts-example-03/ts-example-03/app.ts
+++ ts-example-03/ts-example-03/app.ts
@@ -21,8 +21,25 @@
 
 }
 
+class RandomColorGenerator {
+    nextRandomColor() {
+        // http://stackoverflow.com/questions/1484506/random-color-generator-in-javascript
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+}
+
 window.onload = () => {
     var el = document.getElementById('content');
     var greeter = new Greeter(el);
     greeter.start();    
+
+    window.onclick = function (clickEvent) {
+        var randomColorGenerator = new RandomColorGenerator();
+        el.parentElement.style.backgroundColor = randomColorGenerator.nextRandomColor();
+    }
 };
--- ts-example-03/ts-example-03/javascriptWithRuntimeDetectedErrors.js
+++ ts-example-03/ts-example-03/javascriptWithRuntimeDetectedErrors.js
@@ -1,4 +1,4 @@
-var Greeter = (function () {
+﻿var Greeter = (function () {
     function Greeter(element) {
         this.element = element;
         this.element.innerHTML += "The time is: ";
@@ -6,7 +6,7 @@ var Greeter = (function () {
         this.element.appendChild(this.span);
         this.span.innerText = new Date().toUTCString();
     }
-    Greeter.prototype.start = function () {
+    Greeter.prototype.startTimer = function () {
         var _this = this;
         this.timerToken = setInterval(function () { return _this.span.innerHTML = new Date().toUTCString(); }, 500);
     };
@@ -15,6 +15,7 @@ var Greeter = (function () {
     };
     return Greeter;
 }());
+
 var RandomColorGenerator = (function () {
     function RandomColorGenerator() {
     }
@@ -29,13 +30,22 @@ var RandomColorGenerator = (function () {
     };
     return RandomColorGenerator;
 }());
+
 window.onload = function () {
     var el = document.getElementById('content');
     var greeter = new Greeter(el);
     greeter.start();
+
+    // Intellisense on greeter class
+    // 1. no timer token property
+    // 2. excess properties/methods with not a lot of clear distinction between
+    // 3. renaming start method on greeter class to startTimer doesn't show error until runtime
+
     window.onclick = function (clickEvent) {
         var randomColorGenerator = new RandomColorGenerator();
-        el.parentElement.style.backgroundColor = randomColorGenerator.nextRandomColor();
+        el.parentElement.style.backgroundclar = randomColorGenerator.nextRandomColor();
     };
+
+    // misspelling background color triggers no error
 };
 //# sourceMappingURL=app.js.map
