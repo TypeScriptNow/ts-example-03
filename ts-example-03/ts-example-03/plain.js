45344d40489f56123d588c9bdf04793d9894f88d
Brendan Long <blong145@gmail.com>
2017-02-11 09:33:40 -0600

    Added first part of Intellisense for Typescript demo.

---
+++ ts-example-03/ts-example-03/plain.js
@@ -0,0 +1,27 @@
+﻿var Greeter = (function () {
+    function Greeter(element) {
+        this.element = element;
+        this.element.innerHTML += "The time is: ";
+        this.span = document.createElement('span');
+        this.element.appendChild(this.span);
+        this.span.innerText = new Date().toUTCString();
+    }
+    Greeter.prototype.start = function () {
+        var _this = this;
+        this.timerToken = setInterval(function () { return _this.span.innerHTML = new Date().toUTCString(); }, 500);
+    };
+    Greeter.prototype.stop = function () {
+        clearTimeout(this.timerToken);
+    };
+    return Greeter;
+}());
+
+window.onload = function () {
+    var el = document.getElementById('content');
+    var greeter = new Greeter(el);
+    greeter.
+
+    // Intellisense on greeter class
+    // no timer token property
+    // excess properties/methods with not a lot of clear distinction between
+};
